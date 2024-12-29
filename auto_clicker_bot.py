b8525a8963d6428f28c05ed00ce140f4c8a0bc3f
gehad-eng <gehaddahman@gmail.com>
2024-12-29 16:57:28 +0700

    uploding final project

---
+++ auto_clicker_bot.py
@@ -0,0 +1,36 @@
+import time
+import threading
+from pynput.mouse import Button, Controller
+from pynput.keyboard import Listener, KeyCode
+
+
+TOGGLE_KEY = KeyCode(char='t')
+
+
+clicking = False
+mouse = Controller()
+
+
+def clicker():
+    while True:
+        if clicking:
+            mouse.click(Button.left,1)
+        time.sleep(0.001)
+
+
+
+def toggle_event(key):
+    if key == TOGGLE_KEY:
+        global clicking
+        clicking = not clicking
+        if clicking:
+            print("Auto clicker started")
+        else:
+            print("Auto clicker stopped")
+
+
+click_thread = threading.Thread(target=clicker)
+click_thread.start()
+
+with Listener(on_press=toggle_event) as listener:
+    listener.join()
